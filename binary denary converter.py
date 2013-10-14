78199d7d4069c45b7f749b0e9700feeadc40891e
danielrees97 <danielrees64@yahoo.co.uk>
2013-10-14 10:29:04 +0100

    Binary Denary Convertor

--- binary denary converter.py
+++ binary denary converter.py
@@ -10,14 +10,16 @@ while not exityn:
     while binary_error:
         binary_number= input("Please enter the 8-BIT Binary number you wish to convert: ")
         original_binary_nunber= binary_number
+
         if len(binary_number) >=1 and len(binary_number) <=8:
             binary_error=False
+        elif binary_number [0] != "0" or binary_number [0] != "1":
+            binary_error=True
+            print("-ERROR- Binary word is incorrect, only use Base 2 Values")
         else:
             binary_error=True
             print("-ERROR- Binary word is of incorrect length! ")
-    print("length check passed. ")
 
-    
-    
+        print("Error checking passed. ")
 
     
